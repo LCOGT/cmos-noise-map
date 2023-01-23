6afb1b51f6d43ce903fe50ea3d918df8d4ccf7da
PK0207 <30590837+PK0207@users.noreply.github.com>
2023-01-23 10:11:00 -0800

    Readnoise function and docs updates
    Added a function to go from get_rts parameters to read noise of a  pixel.  Changed some documentation in get_rts, to reflect the new variable of the mixture weights.

--- cmos-noise-map/get_rts.py
+++ cmos-noise-map/get_rts.py
@@ -33,9 +33,12 @@ def data_to_pixel(data):
     x, y = np.meshgrid(range(dshape[1]),range(dshape[2]), indexing = 'ij')
     pixels = np.reshape(np.transpose(data), (dshape[2]*dshape[1], dshape[0])).tolist()
     return pixels
-def readnoise(means, p):
-    readnoise = 1 #https://stats.stackexchange.com/questions/16608/what-is-the-variance-of-the-weighted-mixture-of-two-gaussians
+
+def readnoise(means, variances, amplitudes):
+    var = np.sum(np.dot(amplitudes, variances)) + np.sum(np.dot(amplitudes, means**2)) - np.sum(np.dot(amplitudes, means))**2
+    readnoise = np.sqrt(var)
     return readnoise
+
 def get_rts(p, tol = 0.05, upper_q = 3, min_peak_sep = 10):
     """
     Uses a Gaussian Mixture model, which is essentially a series of gaussian distributions of different means, 
@@ -62,16 +65,14 @@ def get_rts(p, tol = 0.05, upper_q = 3, min_peak_sep = 10):
 
     Returns
     -------
-    peak_location : TYPE: list(int), length = num_peaks
+    peak_location : TYPE: list(float), length = num_peaks
         DESCRIPTION: The means of each of the Gaussian modes calculated by GMM
-    peak_widths : TYPE: list(int), length = num_peaks
+    peak_widths : TYPE: list(float), length = num_peaks
         DESCRIPTION: The standard deviations of each Gaussian mode calculated by GMM
-    peak_distance : TYPE: list(int), length = num_peaks-1
-        DESCRIPTION: The difference between the means of each sequential mode
     num_peaks : TYPE: int
         DESCRIPTION: The number of Gaussians used to model the distribution of values of the pixel
-    bad_idx_gmm : TYPE: list(int), shape(1,2)
-        DESCRIPTION: The python array (not ds9) coordinate of the pixel modeled on the detector
+    amp : TYPE: list(float)
+        DESCRIPTION: The weights of each gaussian in the mixture. All weights sum to 1.
 
     """
     n_clusters=np.arange(1, 4)
@@ -93,46 +94,56 @@ def get_rts(p, tol = 0.05, upper_q = 3, min_peak_sep = 10):
         if n_components == 2:
             peaks = fit_funs[1].means_
             variances = fit_funs[1].covariances_
+            amplitudes = fit_funs[1].weights_
             
             #If peaks are separated enough, then the fit continues
             if np.abs(peaks[1]-peaks[0]) > min_peak_sep: #This is a bad criterion, change
                 peak_location=peaks
                 peak_widths=variances
                 num_peaks=len(peaks)
+                amp = amplitudes
             
             #If they are not well seperated, then choose to model with n=1
             elif np.abs(peaks[1]-peaks[0]) < min_peak_sep:
                 peaks = fit_funs[0].means_
                 variances = fit_funs[0].covariances_
+                amplitudes = fit_funs[0].weights_
                 peak_location=peaks
                 peak_widths=variances
                 num_peaks=len(peaks)
+                amp = amplitudes
         
         elif n_components == 3:
             peaks = fit_funs[2].means_
             variances = fit_funs[2].covariances_
+            amplitudes = fit_funs[2].weights_
             #If peaks are well separated, continue to model with n=3
             if np.abs(peaks[2]-peaks[1]) >= min_peak_sep and np.abs(peaks[1]-peaks[0]) >= min_peak_sep:
                 peak_location=peaks
                 peak_widths=variances
                 num_peaks=len(peaks)
+                amp=amplitudes
             
             #If peaks are not well separated try to model with n=1
             else:
                 peaks = fit_funs[0].means_
                 variances = fit_funs[0].covariances_
+                amplitudes = fit_funs[0].weights_
                 peak_location=peaks
                 peak_widths=variances
                 num_peaks=len(peaks)
+                amp=amplitudes
         #If n=2 and n=3 modesl are not appropriate, model with one component.
         elif n_components == 1:
             peaks = fit_funs[0].means_
             variances = fit_funs[0].covariances_
+            amplitudes = fit_funs[0].weights_
             peak_location=peaks
             peak_widths=variances
             num_peaks=len(peaks)
+            amp=amplitudes
     #If pixel is not noisy, return nans to maintain data structure
     else:
-        peak_location, peak_widths, num_peaks = [np.nan, np.nan, np.nan]
+        peak_location, peak_widths, num_peaks, amp = [np.nan, np.nan, np.nan, np.nan]
     #!TODO: convert to read noise per pixel. Maybe build a separate function to do that. Or puit everything together into a class.
-    return peak_location, peak_widths, num_peaks
+    return peak_location, peak_widths, num_peaks, amp
