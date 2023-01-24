c154f118bfa1c82ee1b43ef3352d49dabe37bdd5
Prerana Kottapalli <pkottapalli@intern06-linux.lco.gtn>
2023-01-24 14:04:55 -0800

    Three main functions are present. The first calculates read noise from rts parameters, the second calculates read noise from just the standard deviation of each pixel, and the third just calculates the rts parameters to return.

--- cmos-noise-map/__main__.py
+++ cmos-noise-map/__main__.py
@@ -4,46 +4,80 @@ Created on Mon Jan 23 11:07:17 2023
 
 @author: prera
 """
-from get_rts import readnoise, get_rts
-from astropy.io import fits
-from glob import glob
+from get_rts import readnoise, get_rts, data_to_pixel
+from read_write_fns import read_bias_frames, write_hdu
 import numpy as np
+    
+#Need at least 50 images
 
-files = glob('/run/media/pkottapalli/Untitled/411_mod3_gain30_bin1_telegraph/*.fits', recursive = True) #Need at least 50
-
-def per_pixel_readnoise(p, tol = 0.05, upper_q = 3, min_peak_sep = 10):
+def per_pixel_readnoise(p, tol=0.05, upper_q = 3, min_peak_sep = 10):
     means, variances, num_peaks, amplitudes = get_rts(p, tol = 0.05, upper_q = 3, min_peak_sep = 10)
     pixel_readnoise = readnoise(means, variances, num_peaks, amplitudes)
     return pixel_readnoise
 
-#Collect images to open
-im_test = fits.open(files[0])[0].data
-data = []
-shape = np.shape(im_test)
-for t in glob('/run/media/pkottapalli/Untitled/bias_temp/*', recursive = True):
-    temp = np.memmap(t, dtype = im_test.dtype , mode = 'r', shape = (1,shape[1]))
-    data.append(temp)
-    del temp
-
-#memory map the bias images in
-
 #read in one row at a time, iterate over it pixel by pixel
+def do_rts(path, *args):
+    ims = read_bias_frames(path)
+    im_test = ims[0].data
+    dshape = np.shape(im_test)
+    readnoise_map = np.zeros((dshape[0], dshape[1]))
+    
+    for row_no in range(0,dshape[0]):
+        data = []
+        for im in ims:
+            data.append(im.data[row_no,:]+32768)
+            
+        #convert data to stacked pixels
+        pixels = data_to_pixel(data)
+        stdimage = np.std(data, axis=0)
+        upper_q = np.quantile(stdimage, 0.8)
+        
+        #Append it all to a pixel map
+        for i, p in enumerate(pixels):
+            noise = per_pixel_readnoise(p, upper_q)
+            if not np.isnan(noise):
+                readnoise_map[row_no,i] = noise
+            else:
+                readnoise_map[row_no, i] = np.std(p)
+    return readnoise_map
 
-dshape = np.shape(data)
-x, y = np.meshgrid(range(dshape[1]),range(dshape[2]), indexing = 'ij')
-indices = np.column_stack((x[-1].ravel(),y[-1].ravel())).tolist()
-pixels = np.reshape(np.transpose(data), (dshape[2]*dshape[1], dshape[0])).tolist()
+def do_std(path, *args):
+    ims = read_bias_frames(path)
+    im_test = ims[0].data
+    dshape = np.shape(im_test)
+    readnoise_map = np.zeros((dshape[0], dshape[1]))
+    for row_no in range(0, dshape[0]):
+        data = []
+        for im in ims:
+            data.append(im.data[row_no,:]+32768)
+            
+        #convert data to stacked pixels
+        stdimage = np.std(data, axis=0)
+        readnoise_map[row_no, :] = stdimage
+    return readnoise_map
 
-stdimage = np.std(data, axis=0)
-upper_q = np.quantile(stdimage, 0.8)
+def do_rts_params(path, *args):
+    ims = read_bias_frames(path)
+    im_test = ims[0].data
+    dshape = np.shape(im_test)
+    param_map = []
+    
+    for row_no in range(0,dshape[0]):
+        data = []
+        for im in ims:
+            data.append(im.data[row_no,:]+32768)
+            
+        #convert data to stacked pixels
+        pixels = data_to_pixel(data)
+        stdimage = np.std(data, axis=0)
+        upper_q = np.quantile(stdimage, 0.8)
+        
+        #Append it all to a pixel map
+        for i, p in enumerate(pixels):
+            params = get_rts(p, upper_q, *args)
+            param_map.append(params)
+    return param_map
 
-
-#Append it all to a pixel map
-readnoise_map = np.zeros((dshape[1], dshape[2]))
-row_no = 0
-for i, p in enumerate(pixels):
-    noise = per_pixel_readnoise(p, upper_q)
-    if not np.isnan(noise):
-        readnoise_map[row_no,i] = noise
-    else:
-        readnoise_map[row_no, i] = np.std(p)
+path = '/run/media/pkottapalli/Untitled/411_mod3_gain30_bin1_telegraph/*.fits'
+readnoise_map = do_std(path)
+write_hdu(readnoise_map)
