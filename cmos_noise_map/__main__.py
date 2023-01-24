622fa9ff7d754ad46d239355686b7e85f63ac5ec
Prerana Kottapalli <pkottapalli@intern06-linux.lco.gtn>
2023-01-24 15:36:14 -0800

    renamed top level file to follow conventions, and ran black on all the files to lint code

---
+++ cmos_noise_map/__main__.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 11:07:17 2023
+
+@author: prera
+"""
+from get_rts import readnoise, get_rts, data_to_pixel
+from read_write_fns import read_bias_frames, write_hdu
+import numpy as np
+
+# Need at least 50 images
+
+
+def per_pixel_readnoise(p, tol=0.05, upper_q=3, min_peak_sep=10):
+    means, variances, num_peaks, amplitudes = get_rts(
+        p, tol=0.05, upper_q=3, min_peak_sep=10
+    )
+    pixel_readnoise = readnoise(means, variances, num_peaks, amplitudes)
+    return pixel_readnoise
+
+
+# read in one row at a time, iterate over it pixel by pixel
+def do_rts(path, *args):
+    ims = read_bias_frames(path)
+    im_test = ims[0].data
+    dshape = np.shape(im_test)
+    readnoise_map = np.zeros((dshape[0], dshape[1]))
+
+    for row_no in range(0, dshape[0]):
+        data = []
+        for im in ims:
+            data.append(im.data[row_no, :] + 32768)
+
+        # convert data to stacked pixels
+        pixels = data_to_pixel(data)
+        stdimage = np.std(data, axis=0)
+        upper_q = np.quantile(stdimage, 0.8)
+
+        # Append it all to a pixel map
+        for i, p in enumerate(pixels):
+            noise = per_pixel_readnoise(p, upper_q)
+            if not np.isnan(noise):
+                readnoise_map[row_no, i] = noise
+            else:
+                readnoise_map[row_no, i] = np.std(p)
+    return readnoise_map
+
+
+def do_std(path, *args):
+    ims = read_bias_frames(path)
+    im_test = ims[0].data
+    dshape = np.shape(im_test)
+    readnoise_map = np.zeros((dshape[0], dshape[1]))
+    for row_no in range(0, dshape[0]):
+        data = []
+        for im in ims:
+            data.append(im.data[row_no, :] + 32768)
+
+        # convert data to stacked pixels
+        stdimage = np.std(data, axis=0)
+        readnoise_map[row_no, :] = stdimage
+    return readnoise_map
+
+
+def do_rts_params(path, *args):
+    ims = read_bias_frames(path)
+    im_test = ims[0].data
+    dshape = np.shape(im_test)
+    param_map = []
+
+    for row_no in range(0, dshape[0]):
+        data = []
+        for im in ims:
+            data.append(im.data[row_no, :] + 32768)
+
+        # convert data to stacked pixels
+        pixels = data_to_pixel(data)
+        stdimage = np.std(data, axis=0)
+        upper_q = np.quantile(stdimage, 0.8)
+
+        # Append it all to a pixel map
+        for i, p in enumerate(pixels):
+            params = get_rts(p, upper_q, *args)
+            param_map.append(params)
+    return param_map
+
+
+path = "/run/media/pkottapalli/Untitled/411_mod3_gain30_bin1_telegraph/*.fits"
+readnoise_map = do_std(path)
+write_hdu(readnoise_map)
---
+++ cmos_noise_map/tests/test_get_rts.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 20 16:00:36 2023
+
+@author: pkottapalli
+"""
+import numpy as np
+from get_rts import get_rts, readnoise
+
+
+def get_rts_test(test_data):
+    """
+    Takes the test_data.npy file in the tests directory, runs the get_rts
+    function on it, and matches peak locations and number of peaks.
+
+    Parameters
+    ----------
+    test_data : float, array(any,N,N)
+        A mock pixel array, with varying amounts of samples and modality.
+
+    Returns
+    -------
+    None.
+
+    """
+    means = []
+    num_peaks = []
+    for p in test_data.flatten():
+        mean, _, num, _ = get_rts(p, tol=0.03, upper_q=10, min_peak_sep=10)
+        means.append(mean)
+        num_peaks.append(num)
+
+    # Check that number of peaks are the same
+    true_peaks = [2, 3, 1, 3, 2, 3]
+    test_peaks = np.array(num_peaks)[~np.isnan(num_peaks)]
+    if len(true_peaks) == len(test_peaks):
+        if np.array(test_peaks - true_peaks).all() == 0:
+            num_peak_test = True
+        else:
+            num_peak_test = False
+
+    # Check that means are close
+    true_means = [
+        [100, 10],
+        [10, 100, 190],
+        [100],
+        [10, 100, 190],
+        [10, 80],
+        [10, 100, 190],
+    ]  # Taken from data in make_test_data
+    test_means = []
+    for i in means:
+        if not np.isnan(i).all():
+            test_means.append(i.flatten())
+    if len(true_means) == len(test_means) and num_peak_test == True:
+        for i in range(len(true_means)):
+            test_bools = np.isclose(
+                np.sort(true_means[i]), np.sort(test_means[i]), atol=6
+            )  # 6 chosen so that fake data passes tests, and because it is less than min peak separation.
+            if test_bools.all() == True:
+                means_test = True
+            else:
+                means_test = False
+
+    # Both tests need to pass for overall pass
+    if num_peak_test == True and means_test == True:
+        print("get_rts.py test passed!")
+    else:
+        print("get_rts.py test failed")
+
+
+def readnoise_test(test_data):
+    """
+    Takes one UNIMODAL pixel in the test_data.npy file in the tests directory, runs the readnoise
+    function on it, and compares the true and calculated standard deviations.
+
+    Parameters
+    ----------
+    test_data : float, array(any,N,N)
+        A mock pixel array, with varying amounts of samples and modality.
+
+    Returns
+    -------
+    None.
+
+    """
+    # Test that variance of a unimodal distribution is still the same
+    mean, variance, num_peaks, amps = get_rts(test_data[0, 3])
+    noise = readnoise(mean, variance, num_peaks, amps)
+    true_noise = 60
+    if np.abs(noise - true_noise) < 1:
+        print("readnoise test passed!")
+    else:
+        print("readnoise test failed")
+
+
+data = np.load("tests/test_data.npy", allow_pickle=True)
+get_rts_test(data)
+readnoise_test(data)
