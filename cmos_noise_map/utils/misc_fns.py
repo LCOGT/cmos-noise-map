f98ea4d1deb88c4b311573c9b0ac35a3a76459ed
Prerana Kottapalli <pkottapalli@intern06-linux.lco.gtn>
2023-01-24 16:27:47 -0800

    moved data_to_pixel to this file because it is a utility function

---
+++ cmos_noise_map/utils/misc_fns.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 24 16:06:54 2023
+
+@author: pkottapalli
+"""
+
+import numpy as np
+
+
+# Assumes that the data is being read into the system in order.
+def data_to_pixel(data):
+    """
+    Takes data in the form of a series of opened images (of whatever shape) and
+    returns the data as a list of pixel values across images, along with data.
+
+    Parameters
+    ----------
+    data : list, shape(N images, rows, columns) #Is this true?
+        DESCRIPTION. A list of images or cutout of images
+
+    Returns
+    -------
+    pixels : list of shape(1, N images)
+        DESCRIPTION. The list of pixel values across images, for all pixels given.
+
+    """
+    dshape = np.shape(data)
+    x, y = np.meshgrid(range(dshape[1]), range(1), indexing="ij")
+    pixels = np.reshape(np.transpose(data), (1 * dshape[1], dshape[0]))
+    return pixels
